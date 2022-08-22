53e534e08fa5e9c72c4fd99629631808eac2a787
KatKlee <katklee2022@gmail.com>
2022-08-22 15:24:43 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function tageImMonat(month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+
+console.log(tageImMonat(1, 2016))
+// 31
+
+console.log(tageImMonat(2, 2016));
+// 29
+
+console.log(tageImMonat(2, 2017))
+// 28
+
+console.log(tageImMonat(12, 2017))
+// 31
